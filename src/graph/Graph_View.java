64f98a89c38d3479e479dbd4c48f7d549d9f72d8
50316004 <f0316004@ipc.shizuoka.ac.jp>
2015-01-27 12:34:25 +0900

    20150126 submit

---
+++ src/graph/Graph_View.java
@@ -0,0 +1,93 @@
+package graph;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class Graph_View extends Frame implements ActionListener, WindowListener {
+
+	public Graph_View(){
+		addWindowListener(this);
+		setTitle("Graph");
+		DefaultCategoryDataset data = new DefaultCategoryDataset();
+		data.addValue(300, "USA", "2005year");
+		data.addValue(500, "USA", "2006year");
+		data.addValue(120, "USA", "2007year");
+
+		data.addValue(200, "china", "2005year");
+		data.addValue(400, "china", "2006year");
+		data.addValue(320, "china", "2007year");
+
+	    JFreeChart chart = 
+	      ChartFactory.createLineChart("import volue",
+	                                   "year",
+	                                   "Ton(t)",
+	                                   data,
+	                                   PlotOrientation.VERTICAL,
+	                                   true,
+	                                   false,
+	                                   false);
+
+	    ChartPanel cpanel = new ChartPanel(chart);
+	    add(cpanel, BorderLayout.CENTER);
+
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
